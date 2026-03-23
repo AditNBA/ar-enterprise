9146fb7e4d54bd0c8a0912cb39563ee712e2f4c4
Aditya <pathakaditya270@gmail.com>
2026-03-23 23:20:35 +0530

    Final build fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"; // Must have this
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(), // Must have this
+  ],
 });
